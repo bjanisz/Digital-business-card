3639150eb3a7b572c296f211a3b25296a62d6c03
Bartosz Janiszewski <b.janiszewski@interia.pl>
2022-08-02 09:23:05 +0200

    fixed spelling issue

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+function About () {
+    return (
+        <div id="about-section">
+            <h3>About</h3>
+            <p>I am an ambitious, meticulous and committed person, who wants to develop constantly. My strong 
+            features include creativeness, great interpersonal skills as well as making contacts easily. I do not hesitate to make hard decisions and always strive to perform in the best way possible.</p>
+        </div>
+    )
+};
+
+export default About
